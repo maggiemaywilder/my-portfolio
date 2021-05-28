84b315f297a436ce56b6581f15f45be4982af232
Maggie May Wilder <maggiemaywilder@gmail.com>
2021-05-28 16:49:33 -0500

    replace contact until form is functional

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,12 +10,12 @@ const Contact = ({ alt, pic }) => {
     return (
         <div className="justify-content-center">
             <Form className='mx-auto' >
-                <Row className='my-5 text-center'>
+                <Row className='my-5 justify-content-center'>
                     <Pic alt={alt} pic={pic}  />  
                 </Row>
                 <Row className="text-center"><h1>{IoIosConstruct}</h1></Row>
                 <Row className='text-center'>
-                    <h3>Please feel free to reach out via email at <a href="maggiemaywilder@gmail.com">maggiemaywilder@gmail.com</a> or through  any of the methods linked at the top of the page while I construct a contact form.</h3>
+                    <h3 className="px-5">Please feel free to reach out via email at <a href="maggiemaywilder@gmail.com">maggiemaywilder@gmail.com</a> or through  any of the methods linked at the top of the page while I construct a contact form.</h3>
                 </Row>
               
             </Form>
